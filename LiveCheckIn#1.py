efcf4060869be5b20615f47ba6766efb6ee0df04
UtkuEfeAyan <76896911+UtkuEfeAyan@users.noreply.github.com>
2026-04-20 17:23:38 -0700

    Add session6prompt.ipynb with sine and cosine calculations and output formatting

---
+++ LiveCheckIn#1.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+
+
+def main():
+    x = 0.0 #float
+    for i in range(20):
+        x = float(2 * i + 19)
+        print(f"The value of x = {x}")
+
+
+if __name__ == "__main__":
+    main()
